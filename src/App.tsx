19684f8435ae327b5673709144f1214657898b8d
heraldofortuna <heraldofortuna@gmail.com>
2022-08-25 21:08:09 -0500

    Se creo una composicion de carpetas adecuada para esta app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Cabedex</h1>
+      <button>Get all Pokemons</button>
+    </div>
+  );
+};
+
+export default App;
